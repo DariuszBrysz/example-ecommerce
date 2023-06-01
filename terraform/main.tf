2945ffe12ed82a44b11ca1bd1c7c11283bbc4623
Dariusz Brysz <dariusz.brysz@q-perior.com>
2023-06-01 12:58:43 +0200

    Added app service

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,24 @@ terraform {
     }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "MyResourceGroup"
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
   location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "myWebApp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
