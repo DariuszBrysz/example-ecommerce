dd54a38e6bd3cb11d4e191775f8bcb352dc62670
Dariusz Brysz <dariusz.brysz@q-perior.com>
2023-06-02 10:16:28 +0200

    Added storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,18 @@ resource "azurerm_sql_database" "example" {
   location            = azurerm_resource_group.example.location
   edition             = "Basic"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                      = "mystorageaccount"
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = azurerm_resource_group.example.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+}
+
+resource "azurerm_storage_container" "example" {
+name                  = "mycontainer"
+storage_account_name  = azurerm_storage_account.example.name
+container_access_type = "private"
+}
