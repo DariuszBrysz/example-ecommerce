ae4bf96768eeced28f62d0bc43fe1f29f3328e2c
Dariusz Brysz <dariusz.brysz@q-perior.com>
2023-06-02 10:21:41 +0200

    Updated name

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_sql_database" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                      = "mystorageaccountdbszkolenie"
+  name                      = "mystoragedbszkolenie"
   resource_group_name       = azurerm_resource_group.example.name
   location                  = azurerm_resource_group.example.location
   account_tier              = "Standard"
