0b145e2ff98958749f0a831429b707c5c07272b0
Dariusz Brysz <dariusz.brysz@q-perior.com>
2023-06-02 09:48:03 +0200

    Added connection to Azure SQL

--- backend/Program.cs
+++ backend/Program.cs
@@ -14,8 +14,7 @@ IConfiguration config = builder.Configuration
     .AddEnvironmentVariables()
     .Build();
 builder.Services.AddSingleton(config);
-// TODO
-builder.Services.AddDbContext<BackendDbContext>(opt => opt.UseInMemoryDatabase("mock"));
+builder.Services.AddDbContext<BackendDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
